6e0e977cffc493d06130dfb4c085eee09b698c62
edris gemtessa <edogemtessa@gmail.com>
2021-06-05 09:29:18 -0500

    js code

--- Leaflet-step-1/static/js/logic.js
+++ Leaflet-step-1/static/js/logic.js
@@ -22,3 +22,32 @@ function getColor(depth) {
     }
 }
 
+//  map
+function drawMap(earthquakes) {
+    
+    // Create the tile layer to be the background the map
+    var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "outdoors-v11",
+        accessToken: API_KEY
+    });
+
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
+    });
+
+    // Create a baseMaps 
+    var baseMaps = {
+        "Geography Map": outdoorsMap,
+        "Light Map": lightmap
+    };
+
+    // overlayMaps to hold the earthquake layer
+    var overlayMaps = {
+        "Earthquakes": earthquakes
+    };
+  
